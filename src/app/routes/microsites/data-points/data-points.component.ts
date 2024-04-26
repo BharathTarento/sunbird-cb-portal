725f95fd9d453caaa6d11d2d7e5a7f0ccb46814e
Venkat Kandagaddala <venky.kandagaddala@gmail.com>
2024-04-26 12:02:15 +0530

    Layout fixes for infra data points

--- src/app/routes/microsites/data-points/data-points.component.ts
+++ src/app/routes/microsites/data-points/data-points.component.ts
@@ -8,12 +8,25 @@ import { Component, Input, OnInit } from '@angular/core';
 export class DataPointsComponent implements OnInit {
   @Input() objectData: any
   @Input() layoutType: string = ''
-  constructor() { 
-    
+
+  
+  constructor() {
+        
   }
 
   ngOnInit() {
+    
   }
+
+  processArray(arr: any[], chunkSize: number): any[][] {
+    const chunkedArray: any[][] = [];
+    for (let i = 0; i < arr.length; i += chunkSize) {
+      chunkedArray.push(arr.slice(i, i + chunkSize));
+    }
+    return chunkedArray;
+  }
+
+  
 
   
   
