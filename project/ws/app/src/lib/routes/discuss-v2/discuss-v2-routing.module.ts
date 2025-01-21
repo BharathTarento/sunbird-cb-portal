9c87706ec22e0fda0fb7e5f3f8d29bca70fa6339
christyfernandes <christyabcd.05@gmail.com>
2025-01-21 11:29:34 +0530

    Task #0000: post details route added

--- project/ws/app/src/lib/routes/discuss-v2/discuss-v2-routing.module.ts
+++ project/ws/app/src/lib/routes/discuss-v2/discuss-v2-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DiscussV2HomeComponent } from './routes/discuss-v2-home/discuss-v2-home.component';
+import { PostDetailsComponent } from './routes/post-details/post-details.component';
 
 const routes: Routes = [
   {
     path: '',
     // loadChildren: () => import('./wrapper/wrapper.module').then(u => u.WrapperModule),
     component: DiscussV2HomeComponent,
+    data: {
+      pageId: 'discussion-forum',
+      module: 'Discuss',
+    },
+  },
+  {
+    path: 'post/:discussionId',
+    // loadChildren: () => import('./wrapper/wrapper.module').then(u => u.WrapperModule),
+    component: PostDetailsComponent,
     data: {
       pageId: 'discussion-forum',
       module: 'Discuss',
