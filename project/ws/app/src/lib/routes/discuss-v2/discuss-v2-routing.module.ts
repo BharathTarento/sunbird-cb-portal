b4bb1be7e91e6e8669ba9c4979edd8f12cf3a044
Bharath Kumar <bharathkumar@TI-MAC-087.local>
2025-01-22 13:48:33 +0530

    changes in the discussion

--- project/ws/app/src/lib/routes/discuss-v2/discuss-v2-routing.module.ts
+++ project/ws/app/src/lib/routes/discuss-v2/discuss-v2-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DiscussV2HomeComponent } from './routes/discuss-v2-home/discuss-v2-home.component';
 import { PostDetailsComponent } from './routes/post-details/post-details.component';
+import { CommunityDetailsHomeComponent } from './routes/community-details-home/community-details-home.component';
 
 const routes: Routes = [
   {
@@ -21,8 +22,18 @@ const routes: Routes = [
       pageId: 'discussion-forum',
       module: 'Discuss',
     },
+  },
+  {
+    path: 'community/:communityId',
+    // loadChildren: () => import('./wrapper/wrapper.module').then(u => u.WrapperModule),
+    component: CommunityDetailsHomeComponent,
+    data: {
+      pageId: 'discussion-forum',
+      module: 'Discuss',
+    },
   }
 ]
+
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
